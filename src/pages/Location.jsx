4b93b92f22a940f9e38efafda4dc84c7291e0fb9
sty0507 <styoung0507@gmail.com>
2022-12-03 04:41:00 +0900

    fix : map 페이지 완성

---
+++ src/pages/Location.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header";
+import Map from "../components/Map";
+
+export function Location() {
+  return (
+    <div>
+      <Header />
+      <Map />
+    </div>
+  );
+}
