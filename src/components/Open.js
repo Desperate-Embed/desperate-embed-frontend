d2d57bbd03c1a696439e55bfecf17ca190f282ab
민석 <80371412+MinSeok0123@users.noreply.github.com>
2022-12-03 00:21:01 +0900

    :apikey

---
+++ src/components/Open.js
@@ -0,0 +1,14 @@
+import "../css/Open.css";
+
+export function Open() {
+  return (
+    <div className="Open">
+      <div className="오픈세로">
+        <span className="하수구관리">하수구 관리</span>
+        <img className="멘홀" src="/image/멘홀.png"></img>
+        <span className="하수구열림">하수구 열림</span>
+        <div></div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
+import axios from "axios";
 
 export function Location() {
+  const [sewerList, setSewerList] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get("/api/sewer/sewerList");
+      setSewerList(data);
+      console.log(data);
+    })();
+  }, []);
+
   return (
     <div>
       <Header />
